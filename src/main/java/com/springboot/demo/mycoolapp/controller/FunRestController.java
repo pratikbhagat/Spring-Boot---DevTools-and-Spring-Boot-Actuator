415b0151cfcc88e61e59740500e738d7ad89ff10
pratik.bhagat <pratik.bhagat@netcorecloud.com>
2022-12-25 18:32:24 +0530

    initial commit

---
+++ src/main/java/com/springboot/demo/mycoolapp/controller/FunRestController.java
@@ -0,0 +1,44 @@
+package com.springboot.demo.mycoolapp.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+		
+	// expose "/" that return "Hello World"
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World! Time on server is " + LocalDateTime.now();
+	}
+	
+	// expose new endpoint for /workout
+	
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		System.out.println("Getting requested");
+		return "Run a hard 5k!";
+	}
+	
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day.";
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
